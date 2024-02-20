fd928c7d1db5cbb61d5f68646bf1472a56bb5e2d
pavannaiik <pavan.naiik1@gmail.com>
2024-02-20 01:06:23 -0500

    static fetch using graphQL

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,24 @@
 const graphql = require('graphql')
+const { GraphQLSchema } = require('graphql');
 
-const {GraphQLObjectType,GraphQLString} = graphql
+const _ = require('lodash')
+
+const {GraphQLObjectType,GraphQLString,GraphQLInt} = graphql
+
+
+// dummy data 
+
+var books =[
+    {name:'Name of the Wind', genre:'Fantasy', id:'1'},
+    {name:'The Final Empire', genre:'Fantasy', id:'2'},
+    {name:'The Long Earth', genre:'Sci-Fi', id:'3'}
+]
+
+var authors =[
+    {name:'Pavan', age:25, id:'1'},
+    {name:'Kalyan', age:26, id:'2'},
+    {name:'Nayak', age:27, id:'3'}
+]
 
 const BookType = new GraphQLObjectType({
     name:'Book',
@@ -9,4 +27,38 @@ const BookType = new GraphQLObjectType({
         name:{type:GraphQLString},
         genre:{type:GraphQLString}
     })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name:'Author',
+    fields:()=>({
+        id:{type:GraphQLString},
+        name:{type:GraphQLString},
+        age:{type:GraphQLInt}
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:{
+        book:{
+            type:BookType,
+            args:{id:{type:GraphQLString}},
+            resolve(parent,args){
+                //code to get the data from database
+                return _.find(books,{id:args.id})
+            }
+        },
+        author:{
+            type:AuthorType,
+            args:{id:{type:GraphQLString}},
+            resolve(parent,args){
+                return _.find(authors,{id:args.id})
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
